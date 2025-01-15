2beee3bafa6a275799dc128c3c4d46fd6da8d3b1
Park Yu Han <pbk2312@naver.com>
2025-01-15 13:18:59 +0900

    feat(repository) - 레포지토리 작성
    - 이메일 회원 찾기 메서드 작성

---
+++ src/main/java/recipe/recipeshare/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package recipe.recipeshare.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import recipe.recipeshare.domain.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
