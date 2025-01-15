a4bb537f4af1e11b0a9f490a966b78c916d63003
Park Yu Han <pbk2312@naver.com>
2025-01-15 13:28:56 +0900

    feat(dto) - 회원가입 Dto 작성
    - 닉네임
    - 이메일
    - 비밀번호
    - 확인 비밀번호

---
+++ src/main/java/recipe/recipeshare/dto/MemberSignUpDto.java
@@ -0,0 +1,29 @@
+package recipe.recipeshare.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class MemberSignUpDto {
+
+    @NotBlank(message = "닉네임은 필수 값입니다.")
+    private String nickName;
+
+    @NotBlank(message = "이메일은 필수 값입니다.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
+    private String password;
+
+    @NotBlank
+    private String checkPassword;
+
+}
