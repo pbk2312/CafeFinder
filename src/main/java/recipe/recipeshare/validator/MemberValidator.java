d96757ef49c7162bcc3add899a6cd37dfbfdfac1
Park Yu Han <pbk2312@naver.com>
2025-01-17 10:46:22 +0900

    chore(validator) : 클래스명 수정으로 인한 import 변경

--- src/main/java/recipe/recipeshare/validator/MemberValidator.java
+++ src/main/java/recipe/recipeshare/validator/MemberValidator.java
@@ -1,13 +1,13 @@
 package recipe.recipeshare.validator;
 
-import recipe.recipeshare.exception.InvalidPasswordException;
+import recipe.recipeshare.exception.PasswordMismatchException;
 
 public class MemberValidator {
 
 
     public static void validatePassword(String password, String checkPassword) {
         if (!password.equals(checkPassword)) {
-            throw new InvalidPasswordException();
+            throw new PasswordMismatchException();
         }
     }
 
