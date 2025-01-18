730a56730fc2b0cb95702a56796da0795b690c29
Park Yu Han <pbk2312@naver.com>
2025-01-18 10:56:40 +0900

    feat(validator) : @Component 추가로 스프링 빈으로 관리

--- src/main/java/recipe/recipeshare/validator/MemberValidator.java
+++ src/main/java/recipe/recipeshare/validator/MemberValidator.java
@@ -1,13 +1,26 @@
 package recipe.recipeshare.validator;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import recipe.recipeshare.exception.MemberAlreadyExistsException;
 import recipe.recipeshare.exception.PasswordMismatchException;
+import recipe.recipeshare.repository.MemberRepository;
 
+@RequiredArgsConstructor
+@Component
 public class MemberValidator {
 
+    private final MemberRepository memberRepository;
 
     public static void validatePassword(String password, String checkPassword) {
         if (!password.equals(checkPassword)) {
             throw new PasswordMismatchException();
+        }
+    }
+
+    public void isMemberExists(String email) {
+        if (memberRepository.findByEmail(email).isPresent()) {
+            throw new MemberAlreadyExistsException();
         }
     }
 
