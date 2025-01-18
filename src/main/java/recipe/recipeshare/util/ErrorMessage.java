818c98c23f89dd90df5076875c1ae1acf6d55658
Park Yu Han <pbk2312@naver.com>
2025-01-18 10:57:23 +0900

    feat(exception) : 예외 메시지 추가

--- src/main/java/recipe/recipeshare/util/ErrorMessage.java
+++ src/main/java/recipe/recipeshare/util/ErrorMessage.java
@@ -11,7 +11,10 @@ public enum ErrorMessage {
     SERVER_ERROR("서버 내부오류"),
     VALIDATION_FAILED("유효성 검증 실패"),
     LOGIN_ERROR_MESSAGE("로그인이 필요한 서비스입니다."),
-    LEVEL_MESSAGE("고객님의 등급은 접근할수없습니다.");
+    LEVEL_MESSAGE("고객님의 등급은 접근할수없습니다."),
+    Member_AlreadyExists("해당 이메일은 이미 존재합니다."),
+    VERIFY_CODE_MIS_MATCH("인증번호가 일치하지 않습니다."),
+    NOT_VERIFY_CODE("이메일 인증이 완료 되지 않았습니다.");
 
     private final String message;
 
