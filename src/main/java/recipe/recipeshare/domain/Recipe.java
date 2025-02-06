7607e4bad77543634a7bbe23a899bd91d0541af8
Park Yu Han <pbk2312@naver.com>
2025-02-06 10:53:54 +0900

    feat(domain) : 조회수 및 평점 추가
    - id를 UUID로 변경

--- src/main/java/recipe/recipeshare/domain/Recipe.java
+++ src/main/java/recipe/recipeshare/domain/Recipe.java
@@ -6,10 +6,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.time.Duration;
 import java.util.List;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,13 +27,18 @@ import lombok.NoArgsConstructor;
 public class Recipe {
 
     @Id
-    private String recipeId;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID recipeId;
 
     @Column(nullable = false)
     private String title;
 
     @Column(nullable = false, length = 1000)
     private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Difficulty difficulty;
 
     @ElementCollection
     @Enumerated(EnumType.STRING)
@@ -39,20 +48,46 @@ public class Recipe {
     private List<String> ingredients;
 
     @Column
-    private Double rating;
-
-    @Column
-    private Integer cookingTime; // 조리 시간 (분 단위)
-
-    @Column
-    private String difficulty; // 난이도 (예: 초급, 중급, 고급)
+    private Duration cookingTime; // 조리 시간 (분 단위)
 
     @Column
     private String imageUrl; // 레시피 이미지 URL
 
-    // 회원과 양방향 관 (N:1)
+    @Column(nullable = false)
+    private Long views = 0L;  // 조회수 (기본값은 0)
+
+    @Column(nullable = false)
+    private Double averageRating = 0.0; // 평점 (기본값은 0.0)
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member author;
 
+    // Recipe 객체 생성 메서드
+    public static Recipe create(String title, String content, Difficulty difficulty, List<FoodType> foodTypes,
+                                List<String> ingredients, Duration cookingTime, String imageUrl, Member author) {
+        return Recipe.builder()
+                .title(title)
+                .content(content)
+                .difficulty(difficulty)
+                .foodTypes(foodTypes)
+                .ingredients(ingredients)
+                .cookingTime(cookingTime)
+                .imageUrl(imageUrl)
+                .author(author)
+                .views(0L)
+                .averageRating(0.0)
+                .build();
+    }
+
+    // 조회수 증가 메서드
+    public void incrementViews() {
+        this.views++;
+    }
+
+    // 평점 업데이트 메서드
+    public void updateRating(double rating) {
+        this.averageRating = (this.averageRating + rating) / 2; // 평점의 평균을 계산
+    }
+    
 }
