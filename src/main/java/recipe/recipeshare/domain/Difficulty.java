cbc9854a76fe541b4cca84605ccf56b70f2e0207
Park Yu Han <pbk2312@naver.com>
2025-02-09 11:31:03 +0900

    remove : 주제 변경으로 인한 삭제

--- src/main/java/recipe/recipeshare/domain/Difficulty.java
+++
@@ -1,5 +0,0 @@
-package recipe.recipeshare.domain;
-
-public enum Difficulty {
-    BEGINNER, INTERMEDIATE, ADVANCED; // 쉬움, 보통 , 어려움
-}
--- src/main/java/recipe/recipeshare/repository/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package recipe.recipeshare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import recipe.recipeshare.domain.Recipe;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
