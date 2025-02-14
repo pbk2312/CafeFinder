369ffe9918b17c4254ec2791069eeed4aa1d1f73
Park Yu Han <pbk2312@naver.com>
2025-02-14 15:05:21 +0900

    feat(config) : 직렬화 설정 추가

--- src/main/java/CafeFinder/cafe/config/RedisConfig.java
+++ src/main/java/CafeFinder/cafe/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -12,7 +13,12 @@ public class RedisConfig {
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+
+        // 직렬화 설정 추가
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+
         return template;
     }
-
+    
 }
