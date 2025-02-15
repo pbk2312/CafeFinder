22ab8b4fcd012b4c2c88fc800a3c41d6bdb4aeb9
Park Yu Han <pbk2312@naver.com>
2025-02-15 13:45:22 +0900

    feat(dto) : 토큰 검증을 위한 dto 작성
    - accessToken 만료 여부
    - refreshToken 만료 여부
    - 새롭게 발급된 accessToken
    - accessToken 만료일

---
+++ src/main/java/CafeFinder/cafe/dto/TokenResultDto.java
@@ -0,0 +1,14 @@
+package CafeFinder.cafe.dto;
+
+import CafeFinder.cafe.jwt.AccesTokenDto;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class TokenResultDto {
+    private boolean isAccessTokenValid;
+    private boolean isRefreshTokenValid;
+    private AccesTokenDto newAccessToken;
+    private String message;
+}
