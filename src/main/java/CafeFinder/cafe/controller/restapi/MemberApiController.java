7d5ae77ba48b9de3aa2a4e4da6fe22adbb4ecd51
Park Yu Han <pbk2312@naver.com>
2025-02-15 13:46:00 +0900

    feat(controller) : 토큰 검증 api 작성
    - 새로 발급 받은 accessToken 존재시 쿠키 저장

--- src/main/java/CafeFinder/cafe/controller/restapi/MemberApiController.java
+++ src/main/java/CafeFinder/cafe/controller/restapi/MemberApiController.java
@@ -3,11 +3,13 @@ package CafeFinder.cafe.controller.restapi;
 
 import static CafeFinder.cafe.util.ViewMessage.LOGIN_SUCCESS;
 import static CafeFinder.cafe.util.ViewMessage.LOGOUT_SUCCESS;
+import static CafeFinder.cafe.util.ViewMessage.NOT_LOGIN;
 import static CafeFinder.cafe.util.ViewMessage.SIGN_UP_SUCCESS;
 
 import CafeFinder.cafe.dto.MemberLoginDto;
 import CafeFinder.cafe.dto.MemberSignUpDto;
 import CafeFinder.cafe.dto.ResponseDto;
+import CafeFinder.cafe.dto.TokenResultDto;
 import CafeFinder.cafe.jwt.AccesTokenDto;
 import CafeFinder.cafe.service.member.MemberService;
 import CafeFinder.cafe.util.CookieUtils;
@@ -16,6 +18,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,6 +60,25 @@ public class MemberApiController {
         return ResponseEntity.ok(new ResponseDto<>(LOGOUT_SUCCESS.getMessage(), null, true));
     }
 
+    @GetMapping("/validateToken")
+    public ResponseEntity<ResponseDto<String>> validateToken(
+            @CookieValue(value = "accessToken", required = false) String accessToken,
+            HttpServletResponse response
+    ) {
+        if (accessToken == null || accessToken.isEmpty()) { // accessToken을 가지고 있지 않으면 비 로그인 상태
+            return ResponseEntity.ok(new ResponseDto<>(NOT_LOGIN.getMessage(), null, false));
+        }
+        TokenResultDto result = memberService.validateToken(accessToken);
+        if (!result.isAccessTokenValid()) { // accessToken이 유효하지 않으면
+            if (result.isRefreshTokenValid()) { // refreshToken 확인 -> 만약 refreshToken이 유효하다면
+                CookieUtils.addCookie(response, "accessToken", result.getNewAccessToken().getAccessToken(),
+                        result.getNewAccessToken().getAccessTokenExpiresIn());
+                return ResponseEntity.ok(new ResponseDto<>(result.getMessage(), null, true));
+            }
+            return ResponseEntity.ok(new ResponseDto<>(result.getMessage(), null, false)); // 유효하지 않다면 false
+        }
+        return ResponseEntity.ok(new ResponseDto<>(result.getMessage(), null, true)); // 원래 accessToken은 유효함
+    }
 
     private String getRedirectUrlFromSession(HttpServletRequest request) {
         String redirectUrl = (String) request.getSession().getAttribute("redirectUrl");
