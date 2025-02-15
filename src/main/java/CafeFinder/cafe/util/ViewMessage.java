7b81422cfb153a287384b9aef248fb2c49bf9b51
Park Yu Han <pbk2312@naver.com>
2025-02-15 13:47:33 +0900

    feat(config) : 로그인 상태 메시지 작성

--- src/main/java/CafeFinder/cafe/util/ViewMessage.java
+++ src/main/java/CafeFinder/cafe/util/ViewMessage.java
@@ -8,6 +8,7 @@ public enum ViewMessage {
     SIGN_UP_SUCCESS("회원가입 성공"),
     LOGIN_SUCCESS("로그인 성공"),
     LOGOUT_SUCCESS("로그아웃 성공"),
+    NOT_LOGIN("비로그인 상태"),
     EMAIL_SEND_SUCCESS("인증번호 이메일 전송 성공"),
     EMAIL_VERIFY_SUCCESS("이메일 인증 성공");
 
