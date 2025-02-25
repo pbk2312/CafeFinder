65ee8a8eecb8e394261f41cbcccdab552331dead
Park Yu Han <pbk2312@naver.com>
2025-02-25 14:28:57 +0900

    feat(config): OAuth2 로그인 기능 통합 및 성공 핸들러 추가
    - 기존 보안 설정에 OAuth2 로그인 기능 통합
    - OAuth2 로그인 성공 시 사용자 정보 처리 및 JWT 토큰 발급을 위한 핸들러 추가

--- src/main/java/CafeFinder/cafe/config/SecurityConfig.java
+++ src/main/java/CafeFinder/cafe/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package CafeFinder.cafe.config;
 
+import CafeFinder.cafe.auth.CustomOAuth2UserService;
+import CafeFinder.cafe.auth.OAuth2AuthenticationSuccessHandler;
 import CafeFinder.cafe.jwt.JwtAccessDeniedHandler;
 import CafeFinder.cafe.jwt.JwtAuthenticationEntryPoint;
 import CafeFinder.cafe.jwt.TokenProvider;
+import CafeFinder.cafe.service.member.MemberService;
+import CafeFinder.cafe.service.redis.RefreshTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -24,6 +28,7 @@ public class SecurityConfig {
     private final TokenProvider tokenProvider;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -31,7 +36,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, OAuth2AuthenticationSuccessHandler successHandler)
+            throws Exception {
         http
                 .csrf(CsrfConfigurer::disable)
 
@@ -51,12 +57,27 @@ public class SecurityConfig {
                         ).permitAll()
                         .anyRequest().permitAll()
                 )
-
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .loginPage("/member/signupAndLogin")
+                        .successHandler(successHandler)
+                        // 로그인 성공 시 사용자 정보 처리
+                        .userInfoEndpoint(userInfoEndpoint ->
+                                userInfoEndpoint.userService(customOAuth2UserService)
+                        )
+                )
                 .with(new JwtSecurityConfig(tokenProvider), jwtSecurityConfig -> {
                 });
 
         return http.build();
     }
+
+    @Bean
+    public OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler(MemberService memberService,
+                                                                                 TokenProvider tokenProvider,
+                                                                                 RefreshTokenService refreshTokenService) {
+        return new OAuth2AuthenticationSuccessHandler(memberService, tokenProvider, refreshTokenService);
+    }
+
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
