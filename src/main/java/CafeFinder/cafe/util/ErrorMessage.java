26726dfd5f84a9e00e406e3076bdcfb4efccd4be
Park Yu Han <pbk2312@naver.com>
2025-02-26 23:10:53 +0900

    feat(util): 예외 메시지 추가

--- src/main/java/CafeFinder/cafe/util/ErrorMessage.java
+++ src/main/java/CafeFinder/cafe/util/ErrorMessage.java
@@ -15,7 +15,8 @@ public enum ErrorMessage {
     Member_AlreadyExists("해당 이메일은 이미 존재합니다."),
     VERIFY_CODE_MIS_MATCH("인증번호가 일치하지 않습니다."),
     NOT_VERIFY_CODE("이메일 인증이 완료 되지 않았습니다."),
-    UnsupportedProvider("소셜로그인 오류(제공자 오류) : ");
+    UNSUPPORTEDPROVIDER("소셜로그인 오류(제공자 오류) : "),
+    PROFILESAVEEXCEPTION("프로필 이미지 저장 중 오류 발생");
 
     private final String message;
 
