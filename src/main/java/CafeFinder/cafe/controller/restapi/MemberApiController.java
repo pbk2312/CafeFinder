6753475bc2752730245a6fde516d35d07092a905
Park Yu Han <pbk2312@naver.com>
2025-02-27 15:15:15 +0900

    feat(controller) : 회원 정보 조회 api 작성 및 회원 수정 api 작성
    -  `update`
    - `profile`

--- src/main/java/CafeFinder/cafe/controller/restapi/MemberApiController.java
+++ src/main/java/CafeFinder/cafe/controller/restapi/MemberApiController.java
@@ -4,13 +4,17 @@ package CafeFinder.cafe.controller.restapi;
 import static CafeFinder.cafe.util.ViewMessage.LOGIN_SUCCESS;
 import static CafeFinder.cafe.util.ViewMessage.LOGOUT_SUCCESS;
 import static CafeFinder.cafe.util.ViewMessage.NOT_LOGIN;
+import static CafeFinder.cafe.util.ViewMessage.PROFILE_INFO;
 import static CafeFinder.cafe.util.ViewMessage.SIGN_UP_SUCCESS;
+import static CafeFinder.cafe.util.ViewMessage.UPDATE_PROFILE;
 
 import CafeFinder.cafe.dto.MemberLoginDto;
 import CafeFinder.cafe.dto.MemberSignUpDto;
+import CafeFinder.cafe.dto.ProfileDto;
 import CafeFinder.cafe.dto.ResponseDto;
 import CafeFinder.cafe.dto.TokenResultDto;
 import CafeFinder.cafe.dto.UserInfoDto;
+import CafeFinder.cafe.dto.UserUpdateDto;
 import CafeFinder.cafe.jwt.AccesTokenDto;
 import CafeFinder.cafe.service.member.MemberService;
 import CafeFinder.cafe.util.CookieUtils;
@@ -22,6 +26,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -63,6 +68,22 @@ public class MemberApiController {
         return ResponseEntity.ok(new ResponseDto<>(LOGOUT_SUCCESS.getMessage(), null, true));
     }
 
+    @PatchMapping("/update")
+    public ResponseEntity<ResponseDto<String>> update(
+            @Valid UserUpdateDto userUpdateDto,
+            @CookieValue(value = "accessToken", required = false) String accessToken
+    ) {
+        memberService.update(userUpdateDto, accessToken);
+        return ResponseEntity.ok(new ResponseDto<>(UPDATE_PROFILE.getMessage(), null, true));
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<ResponseDto<ProfileDto>> getProfile(
+            @CookieValue(value = "accessToken", required = true) String accessToen) {
+        ProfileDto profileDto = memberService.getProfileByToken(accessToen);
+        return ResponseEntity.ok(new ResponseDto<>(PROFILE_INFO.getMessage(), profileDto, true));
+    }
+
     @GetMapping("/validateToken")
     public ResponseEntity<ResponseDto<UserInfoDto>> validateToken(
             @CookieValue(value = "accessToken", required = false) String accessToken,
@@ -86,7 +107,7 @@ public class MemberApiController {
         }
 
         // accessToken을 사용하여 사용자 정보 조회
-        UserInfoDto userInfo = memberService.findUserInfoByToken(accessToken);
+        UserInfoDto userInfo = memberService.getUserInfoByToken(accessToken);
 
         return ResponseEntity.ok(new ResponseDto<>("로그인 성공", userInfo, true));
     }
