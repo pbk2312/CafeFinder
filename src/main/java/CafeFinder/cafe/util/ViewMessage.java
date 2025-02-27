8bc7575cd1b3e0ab55426b5a03bcdca984a54ca7
Park Yu Han <pbk2312@naver.com>
2025-02-27 15:14:05 +0900

    feat(util) : 프로필 메시지 추가
    - `프로필 수정 성공`
    - `프로필 정보 조회 성공`

--- src/main/java/CafeFinder/cafe/util/ViewMessage.java
+++ src/main/java/CafeFinder/cafe/util/ViewMessage.java
@@ -10,7 +10,9 @@ public enum ViewMessage {
     LOGOUT_SUCCESS("로그아웃 성공"),
     NOT_LOGIN("비로그인 상태"),
     EMAIL_SEND_SUCCESS("인증번호 이메일 전송 성공"),
-    EMAIL_VERIFY_SUCCESS("이메일 인증 성공");
+    EMAIL_VERIFY_SUCCESS("이메일 인증 성공"),
+    UPDATE_PROFILE("프로필 수정 성공"),
+    PROFILE_INFO("프로필 정보 조회 성공");
 
     private final String message;
 
