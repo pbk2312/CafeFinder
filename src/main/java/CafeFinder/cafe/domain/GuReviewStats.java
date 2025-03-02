87d1b80c3137a6390ca37c2d95a05918c57a778d
Park Yu Han <pbk2312@naver.com>
2025-03-02 14:47:02 +0900

    feat(domain) : 구별 정보 엔티티 추가
    - 구 코드
    - 평균 평점
    - 총 리뷰수

---
+++ src/main/java/CafeFinder/cafe/domain/GuReviewStats.java
@@ -0,0 +1,28 @@
+package CafeFinder.cafe.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GuReviewStats {
+
+    @Id
+    @Column(name = "gu_code", length = 10, nullable = false)
+    private String guCode;  // 구 코드
+
+    @Column(name = "average_rating", nullable = false)
+    private double averageRating;  // 평균 평점
+
+    @Column(name = "total_reviews", nullable = false)
+    private int totalReviews;  // 총 리뷰 수
+
+}
