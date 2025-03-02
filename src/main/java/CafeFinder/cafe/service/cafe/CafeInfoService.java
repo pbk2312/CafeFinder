d9efe5dc0c6460180fef73c5ba68a56714697102
Park Yu Han <pbk2312@naver.com>
2025-03-02 15:14:47 +0900

    feat(service) : 구별,테마별 검색 로직 작성
    - getCafesByDistrictAndTheme 메서드 작성

--- src/main/java/CafeFinder/cafe/service/cafe/CafeInfoService.java
+++ src/main/java/CafeFinder/cafe/service/cafe/CafeInfoService.java
@@ -1,9 +1,15 @@
 package CafeFinder.cafe.service.cafe;
 
+import CafeFinder.cafe.domain.CafeDistrict;
 import CafeFinder.cafe.domain.CafeInfo;
+import CafeFinder.cafe.domain.CafeTheme;
+import CafeFinder.cafe.dto.CafeInfoDto;
+import CafeFinder.cafe.exception.WrongDistrictAndTheme;
 import CafeFinder.cafe.repository.CafeInfoRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,12 +24,24 @@ public class CafeInfoService {
         cafeInfoRepository.saveAll(cafes);
     }
 
+    @Transactional(readOnly = true)
     public long countCafes() {
         return cafeInfoRepository.count();  // DB에 저장된 카페 개수 확인
     }
 
-    public List<CafeInfo> getAllCafes() {
-        return cafeInfoRepository.findAll(); // 전체 카페 조회
+    // 구별, 테마별 검색 기능
+    @Transactional(readOnly = true)
+    public Page<CafeInfoDto> getCafesByDistrictAndTheme(String district, String theme, Pageable pageable) {
+        try {
+            CafeDistrict cafeDistrict = CafeDistrict.valueOf(district.toUpperCase());
+            CafeTheme cafeTheme = CafeTheme.valueOf(theme.toUpperCase());
+
+            Page<CafeInfo> byDistrictAndTheme = cafeInfoRepository.findByDistrictAndTheme(cafeDistrict, cafeTheme,
+                    pageable);
+            return byDistrictAndTheme.map(CafeInfoDto::fromEntity);
+        } catch (IllegalArgumentException e) {
+            throw new WrongDistrictAndTheme();
+        }
     }
 
 
