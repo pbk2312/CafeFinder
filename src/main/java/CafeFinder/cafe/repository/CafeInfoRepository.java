94d7784744348fd437a5aa3f4859ddbe9cd38849
Park Yu Han <pbk2312@naver.com>
2025-03-02 15:11:42 +0900

    feat(repository) : 구,테마 별로 검색하는 메서드 작성
    - findByDistrictAndTheme

--- src/main/java/CafeFinder/cafe/repository/CafeInfoRepository.java
+++ src/main/java/CafeFinder/cafe/repository/CafeInfoRepository.java
@@ -1,7 +1,15 @@
 package CafeFinder.cafe.repository;
 
+import CafeFinder.cafe.domain.CafeDistrict;
 import CafeFinder.cafe.domain.CafeInfo;
+import CafeFinder.cafe.domain.CafeTheme;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CafeInfoRepository extends JpaRepository<CafeInfo, Long> {
+public interface CafeInfoRepository extends JpaRepository<CafeInfo, String> {
+
+    // 구,테마 별로 검색하기
+    Page<CafeInfo> findByDistrictAndTheme(CafeDistrict district, CafeTheme cafeTheme, Pageable pageable);
+
 }
