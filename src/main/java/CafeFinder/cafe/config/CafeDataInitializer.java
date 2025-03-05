0ba3a432602f6dcb02f41d7d04fccaa3940a5985
parkyuhan <parkyuhan0525@gmail.com>
2025-03-05 10:45:25 +0900

    feat(config) : 리뷰 저장 로직 추가 및 메서드 분리
    - 메서드 분리로 가독성 향상

--- src/main/java/CafeFinder/cafe/config/CafeDataInitializer.java
+++ src/main/java/CafeFinder/cafe/config/CafeDataInitializer.java
@@ -1,7 +1,9 @@
 package CafeFinder.cafe.config;
 
 import CafeFinder.cafe.importer.CafeInfoCsvImporter;
+import CafeFinder.cafe.importer.CafeReviewImporter;
 import CafeFinder.cafe.importer.GuReviewStatsCsvImporter;
+import CafeFinder.cafe.repository.CafeReviewRepository;
 import CafeFinder.cafe.service.cafe.CafeInfoService;
 import CafeFinder.cafe.service.cafe.GuReviewStatsService;
 import java.nio.file.Files;
@@ -22,6 +24,8 @@ public class CafeDataInitializer {
     private final GuReviewStatsService guReviewStatsService;
     private final CafeInfoCsvImporter cafeInfoCsvImporter;
     private final GuReviewStatsCsvImporter guReviewStatsCsvImporter;
+    private final CafeReviewImporter cafeReviewImporter;
+    private final CafeReviewRepository cafeReviewRepository;
 
     @Value("${file.cafeInfo.path}")
     private String cafeInfoPath;
@@ -29,33 +33,60 @@ public class CafeDataInitializer {
     @Value("${file.guReviewStats.path}")
     private String guReviewStatsPath;
 
+    @Value("${file.cafe_reviews.path}") // 리뷰 파일 경로 추가
+    private String cafeReviewsPath;
+
     @Bean
     public ApplicationRunner initializeCafeData() {
         return args -> {
-            // 카페 정보 CSV import
-            if (cafeInfoService.countCafes() == 0) { // DB에 데이터가 없을 때만 실행
-                if (Files.exists(Paths.get(cafeInfoPath))) {
-                    cafeInfoCsvImporter.importCsv(cafeInfoPath);
-                    log.info("카페 정보 CSV import 완료");
-                } else {
-                    log.error("카페 정보 CSV 파일이 존재하지 않습니다: {}", cafeInfoPath);
-                }
-            } else {
-                log.info("DB에 이미 카페 데이터가 존재하므로 CSV Import를 건너뜁니다.");
-            }
+            cafeInfoImport();
 
-            // 구별 리뷰 통계 CSV import
-            if (guReviewStatsService.countGuReviewStats() == 0) { // DB에 데이터가 없을 때만 실행
-                if (Files.exists(Paths.get(guReviewStatsPath))) {
-                    guReviewStatsCsvImporter.importCsv(guReviewStatsPath);
-                    log.info("구별 리뷰 통계 CSV import 완료");
-                } else {
-                    log.error("구별 리뷰 통계 CSV 파일이 존재하지 않습니다: {}", guReviewStatsPath);
-                }
-            } else {
-                log.info("DB에 이미 구별 리뷰 통계 데이터가 존재하므로 CSV Import를 건너뜁니다.");
-            }
+            guReviewStatsImport();
+
+            reviewImport();
         };
+    }
+
+    private void reviewImport() {
+        // 리뷰 데이터 CSV import
+        if (cafeReviewRepository.count() == 0) { // 리뷰 데이터가 없을 때만 실행
+            if (Files.exists(Paths.get(cafeReviewsPath))) {
+                cafeReviewImporter.saveReviewsFromCSV();
+                log.info("리뷰 데이터 CSV import 완료");
+            } else {
+                log.error("리뷰 CSV 파일이 존재하지 않습니다: {}", cafeReviewsPath);
+            }
+        } else {
+            log.info("DB에 이미 리뷰 데이터가 존재하므로 CSV Import를 건너뜁니다.");
+        }
+    }
+
+    private void guReviewStatsImport() {
+        // 구별 리뷰 통계 CSV import
+        if (guReviewStatsService.countGuReviewStats() == 0) { // DB에 데이터가 없을 때만 실행
+            if (Files.exists(Paths.get(guReviewStatsPath))) {
+                guReviewStatsCsvImporter.importCsv(guReviewStatsPath);
+                log.info("구별 리뷰 통계 CSV import 완료");
+            } else {
+                log.error("구별 리뷰 통계 CSV 파일이 존재하지 않습니다: {}", guReviewStatsPath);
+            }
+        } else {
+            log.info("DB에 이미 구별 리뷰 통계 데이터가 존재하므로 CSV Import를 건너뜁니다.");
+        }
+    }
+
+    private void cafeInfoImport() {
+        // 카페 정보 CSV import
+        if (cafeInfoService.countCafes() == 0) { // DB에 데이터가 없을 때만 실행
+            if (Files.exists(Paths.get(cafeInfoPath))) {
+                cafeInfoCsvImporter.importCsv(cafeInfoPath);
+                log.info("카페 정보 CSV import 완료");
+            } else {
+                log.error("카페 정보 CSV 파일이 존재하지 않습니다: {}", cafeInfoPath);
+            }
+        } else {
+            log.info("DB에 이미 카페 데이터가 존재하므로 CSV Import를 건너뜁니다.");
+        }
     }
 
 }
