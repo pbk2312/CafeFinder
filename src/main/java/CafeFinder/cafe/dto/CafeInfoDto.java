52a0ebc7f14076044252f3cadfbeb4fe25ed14d2
parkyuhan <parkyuhan0525@gmail.com>
2025-03-05 17:37:59 +0900

    feat(dto) : 리뷰들 전달하기 위한 dto 필드 추가 및 목록조회와 카페 상세 정보 조회 빌더 메서드 분리
    - CafeReviewDto 리스트 추가
    - 목록조회용과 상세정보조회용 메서드 분리 및 작성

--- src/main/java/CafeFinder/cafe/dto/CafeInfoDto.java
+++ src/main/java/CafeFinder/cafe/dto/CafeInfoDto.java
@@ -3,6 +3,7 @@ package CafeFinder.cafe.dto;
 import CafeFinder.cafe.domain.CafeDistrict;
 import CafeFinder.cafe.domain.CafeInfo;
 import CafeFinder.cafe.domain.CafeTheme;
+import java.util.List;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,9 +26,12 @@ public class CafeInfoDto {
     private Double review;  // 평균 평점
     private Set<CafeTheme> themes; // 여러 개의 카페 테마 저장
 
+    // 목록 조회 때 사용 X
+    private List<CafeReviewDto> reviews;
+
 
     // 엔티티 -> DTO
-    public static CafeInfoDto fromEntity(CafeInfo cafeInfo) {
+    public static CafeInfoDto fromEntityForList(CafeInfo cafeInfo) {
         return CafeInfoDto.builder()
                 .cafeCode(cafeInfo.getCafeCode())
                 .name(cafeInfo.getName())
@@ -38,6 +42,22 @@ public class CafeInfoDto {
                 .imageUrl(cafeInfo.getImageUrl())
                 .review(cafeInfo.getReview())
                 .themes(cafeInfo.getThemes())
+                .build();
+    }
+
+    // 상세 조회용 변환 메서드 (리뷰 목록 포함)
+    public static CafeInfoDto fromEntityWithReviews(CafeInfo cafeInfo, List<CafeReviewDto> reviewDtos) {
+        return CafeInfoDto.builder()
+                .cafeCode(cafeInfo.getCafeCode())
+                .name(cafeInfo.getName())
+                .address(cafeInfo.getAddress())
+                .district(cafeInfo.getDistrict())
+                .hours(cafeInfo.getHours())
+                .phone(cafeInfo.getPhone())
+                .imageUrl(cafeInfo.getImageUrl())
+                .review(cafeInfo.getReview())
+                .themes(cafeInfo.getThemes())
+                .reviews(reviewDtos)
                 .build();
     }
 
