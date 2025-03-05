a12316603bfa44cc8ce7b711ba67e2fc946d7bf2
parkyuhan <parkyuhan0525@gmail.com>
2025-03-05 17:47:55 +0900

    feat(service) : 카페 상세 조회 로직 작성
    - 카페 코드으로 카페 조회
    - 리뷰조회 및 dto 반환

--- src/main/java/CafeFinder/cafe/service/cafe/CafeInfoService.java
+++ src/main/java/CafeFinder/cafe/service/cafe/CafeInfoService.java
@@ -4,10 +4,13 @@ import CafeFinder.cafe.domain.CafeDistrict;
 import CafeFinder.cafe.domain.CafeInfo;
 import CafeFinder.cafe.domain.CafeTheme;
 import CafeFinder.cafe.dto.CafeInfoDto;
+import CafeFinder.cafe.dto.CafeReviewDto;
+import CafeFinder.cafe.exception.CafeInfoNotFoundException;
 import CafeFinder.cafe.exception.WrongDistrictAndTheme;
 import CafeFinder.cafe.repository.CafeInfoRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class CafeInfoService {
 
     private final CafeInfoRepository cafeInfoRepository;
@@ -33,16 +37,30 @@ public class CafeInfoService {
     @Transactional(readOnly = true)
     public Page<CafeInfoDto> getCafesByDistrictAndTheme(String district, String theme, Pageable pageable) {
         try {
+            log.info("district : {} , theme : {} ", district, theme);
             CafeDistrict cafeDistrict = CafeDistrict.valueOf(district.toUpperCase());
             CafeTheme cafeTheme = CafeTheme.valueOf(theme.toUpperCase());
 
             Page<CafeInfo> byDistrictAndTheme = cafeInfoRepository.findByDistrictAndThemesContaining(cafeDistrict,
                     cafeTheme,
                     pageable);
-            return byDistrictAndTheme.map(CafeInfoDto::fromEntity);
+            return byDistrictAndTheme.map(CafeInfoDto::fromEntityForList);
         } catch (IllegalArgumentException e) {
             throw new WrongDistrictAndTheme();
         }
+    }
+
+    // 카페 상세 조회
+    @Transactional(readOnly = true)
+    public CafeInfoDto getCafeInfo(String cafeCode) {
+        CafeInfo cafeInfo = cafeInfoRepository.findByCafeCodeWithReviews(cafeCode)
+                .orElseThrow(() -> new CafeInfoNotFoundException(cafeCode));
+
+        List<CafeReviewDto> reviewDtos = cafeInfo.getReviews().stream()
+                .map(CafeReviewDto::fromEntity)
+                .toList();
+
+        return CafeInfoDto.fromEntityWithReviews(cafeInfo, reviewDtos);
     }
 
 }
