8c301f38761ecb5ff601a8e756fb5b244c76ed86
parkyuhan <parkyuhan0525@gmail.com>
2025-03-05 10:44:56 +0900

    feat(importer) : CSV파일에 있는 리뷰들 DB 적재
    - 평균 평점 부분은 스킵
    - 파싱 로직 사용

---
+++ src/main/java/CafeFinder/cafe/importer/CafeReviewImporter.java
@@ -0,0 +1,41 @@
+package CafeFinder.cafe.importer;
+
+import CafeFinder.cafe.repository.CafeInfoRepository;
+import CafeFinder.cafe.repository.CafeReviewRepository;
+import CafeFinder.cafe.util.CsvParserUtil;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class CafeReviewImporter {
+
+    private final CafeReviewRepository cafeReviewRepository;
+
+    private final CafeInfoRepository cafeInfoRepository;  // Cafe 엔티티도 필요
+
+
+    @Value("${file.cafe_reviews.path}")
+    private String filePath;
+
+    @Transactional
+    public void saveReviewsFromCSV() {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                CsvParserUtil.parseCafeReview(line, cafeInfoRepository)
+                        .ifPresent(cafeReviewRepository::save);
+            }
+        } catch (IOException e) {
+            log.error("리뷰 CSV 파일을 읽는 중 오류 발생: {}", e.getMessage(), e);
+        }
+    }
+
+}
