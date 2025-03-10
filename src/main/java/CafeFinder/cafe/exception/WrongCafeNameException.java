8f9d0f759b2ab62c883ccb5494604d49c8e39a32
parkyuhan <parkyuhan0525@gmail.com>
2025-03-10 10:18:11 +0900

    chore(exception) : 안쓰는 exception 삭제

--- src/main/java/CafeFinder/cafe/exception/WrongCafeNameException.java
+++
@@ -1,9 +0,0 @@
-package CafeFinder.cafe.exception;
-
-import static CafeFinder.cafe.util.ErrorMessage.WRONG_CAFE_NAME;
-
-public class WrongCafeNameException extends RuntimeException {
-    public WrongCafeNameException() {
-        super(WRONG_CAFE_NAME.getMessage());
-    }
-}
