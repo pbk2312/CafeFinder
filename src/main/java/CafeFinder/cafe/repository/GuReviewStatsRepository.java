521d10336f79a4d274b63a132971e4176ba24818
parkyuhan <parkyuhan0525@gmail.com>
2025-03-12 10:24:29 +0900

    refactor(repository): 평점 동일 시 리뷰 수 기준으로 정렬 추가
    - 기존에는 averageRating 기준으로만 정렬하던 것을
    averageRating이 동일할 경우 totalReviews 기준으로 정렬하도록 JPQL 쿼리를 수정

--- src/main/java/CafeFinder/cafe/repository/GuReviewStatsRepository.java
+++ src/main/java/CafeFinder/cafe/repository/GuReviewStatsRepository.java
@@ -3,10 +3,12 @@ package CafeFinder.cafe.repository;
 import CafeFinder.cafe.domain.GuReviewStats;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GuReviewStatsRepository extends JpaRepository<GuReviewStats, String> {
 
-    // 평점 높은 순서대로
-    List<GuReviewStats> findAllByOrderByAverageRatingDesc();
+    // 평점이 높은 순서대로, 평점이 같으면 리뷰 갯수대로
+    @Query("SELECT A FROM GuReviewStats A ORDER BY A.averageRating DESC ,A.totalReviews DESC ")
+    List<GuReviewStats> findSortedByRatingAndReviewCount();
 
 }
