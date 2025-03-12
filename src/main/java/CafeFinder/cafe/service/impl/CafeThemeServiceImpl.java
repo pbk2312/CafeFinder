4c4d71b9d345b3a92e06c6cea3a67179eebebda0
parkyuhan <parkyuhan0525@gmail.com>
2025-03-12 10:17:39 +0900

    refactor(service): Service의 구현체와 인터페이스 분리
    서비스 로직의 결합도를 낮추고 유지보수성을 높이기 위해
    Service 인터페이스와 구현체를 분리

---
+++ src/main/java/CafeFinder/cafe/service/impl/CafeThemeServiceImpl.java
@@ -0,0 +1,25 @@
+package CafeFinder.cafe.service.impl;
+
+import CafeFinder.cafe.domain.CafeTheme;
+import CafeFinder.cafe.dto.ThemeDto;
+import CafeFinder.cafe.service.interfaces.CafeThemeService;
+import java.util.Arrays;
+import java.util.List;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class CafeThemeServiceImpl implements CafeThemeService {
+
+    @Override
+    @Cacheable(value = "cafeThemes")
+    public List<ThemeDto> getCafeThemes() {
+        log.info("테마들 조회");
+        return Arrays.stream(CafeTheme.values())
+                .map(ThemeDto::fromEntity)
+                .toList();
+    }
+
+}
---
+++ src/main/java/CafeFinder/cafe/service/interfaces/CafeThemeService.java
@@ -0,0 +1,10 @@
+package CafeFinder.cafe.service.interfaces;
+
+import CafeFinder.cafe.dto.ThemeDto;
+import java.util.List;
+
+public interface CafeThemeService {
+
+    List<ThemeDto> getCafeThemes();
+
+}
