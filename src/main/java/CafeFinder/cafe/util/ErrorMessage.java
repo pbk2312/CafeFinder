dee6b8d6288b799ca20c86d113a032a0575b9293
parkyuhan <parkyuhan0525@gmail.com>
2025-03-18 15:28:31 +0900

    chore(util) : 상수명 변경

--- src/main/java/CafeFinder/cafe/util/ErrorMessage.java
+++ src/main/java/CafeFinder/cafe/util/ErrorMessage.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum ErrorMessage {
 
     MEMBER_NOT_FOUND("회원이 존재하지 않습니다."),
-    PASSWORDS_DO_NOT_MATCH("두 비밀번호가 일치하지 않습니다."),
+    PASSWORD_CONFIRMATION_MISMATCH("두 비밀번호가 일치하지 않습니다."),
     PASSWORD_INCORRECT("비밀번호가 일치하지 않습니다."),
     SERVER_ERROR("서버 내부오류"),
     VALIDATION_FAILED("유효성 검증 실패"),
