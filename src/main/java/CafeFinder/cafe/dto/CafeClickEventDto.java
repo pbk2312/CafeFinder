65bc36b28f62d6e3c75eef4e7cdbd4e116c5f8eb
parkyuhan <parkyuhan0525@gmail.com>
2025-03-18 15:21:48 +0900

    feat(dto): 카페 클릭 이벤트 DTO 추가

---
+++ src/main/java/CafeFinder/cafe/dto/CafeClickEventDto.java
@@ -0,0 +1,22 @@
+package CafeFinder.cafe.dto;
+
+
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CafeClickEventDto {
+
+    private String memberId;
+    private String cafeCode;
+    private Set<String> themes;
+    private String district;
+    private long timestamp;   // 이벤트 발생 시간 (Epoch millis)
+
+}
