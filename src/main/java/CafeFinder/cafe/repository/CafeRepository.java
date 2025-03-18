8b1dab3d2b3f0235a85a252363d79db70843ef35
parkyuhan <parkyuhan0525@gmail.com>
2025-03-18 09:45:54 +0900

    chore(repository) : 클래스명 변경
    -CafeInfoRepository -> CafeRepository

---
+++ src/main/java/CafeFinder/cafe/repository/CafeRepository.java
@@ -0,0 +1,15 @@
+package CafeFinder.cafe.repository;
+
+import CafeFinder.cafe.domain.Cafe;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface CafeRepository extends JpaRepository<Cafe, String> {
+
+    // N+1 문제 방지
+    @Query("select distinct c from Cafe c left join fetch c.reviews where c.cafeCode = :cafeCode")
+    Optional<Cafe> findByCafeCodeWithReviews(@Param("cafeCode") String cafeCode);
+
+}
