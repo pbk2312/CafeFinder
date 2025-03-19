b6e70d572cbf87c0eb41b3a0928aa9990035d6bd
parkyuhan <parkyuhan0525@gmail.com>
2025-03-19 11:36:33 +0900

    chore(controller) : 클래스명 변경
    - cafeInfoController -> cafeController

--- src/main/java/CafeFinder/cafe/controller/view/CafeController.java
+++ src/main/java/CafeFinder/cafe/controller/view/CafeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/cafe")
-public class CafeInfoController {
+public class CafeController {
 
     @Value("${google.map.key}")
     private String googleKey;
