9844140d0101b1ec0084ec089342c5c0f5393ef7
pbk2312 <pbk2312@inu.ac.kr>
2025-04-11 10:39:12 +0900

    feat: CafeScrap JPA 레포지토리 추가

---
+++ src/main/java/CafeFinder/cafe/repository/CafeScrapRepository.java
@@ -0,0 +1,7 @@
+package CafeFinder.cafe.repository;
+
+import CafeFinder.cafe.domain.CafeScrap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CafeScrapRepository extends JpaRepository<CafeScrap, Long> {
+}
