cd73b5791a4517e25f2f8fab7cf60d87fb0a31bd
pbk2312 <pbk2312@inu.ac.kr>
2025-04-11 10:41:38 +0900

    chore : 필요없는 주석 삭제

--- src/main/java/CafeFinder/cafe/repository/SeoulDistrictRepository.java
+++ src/main/java/CafeFinder/cafe/repository/SeoulDistrictRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface SeoulDistrictRepository extends JpaRepository<SeoulDistrictStatus, String> {
 
-    // 평점이 높은 순서대로, 평점이 같으면 리뷰 갯수대로
     @Query("SELECT A FROM SeoulDistrictStatus A ORDER BY A.averageRating DESC ,A.totalReviews DESC ")
     List<SeoulDistrictStatus> findSortedByRatingAndReviewCount();
 
