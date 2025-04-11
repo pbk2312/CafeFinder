ed9c47e40840d22d61eaba56fb24397594a0b085
pbk2312 <pbk2312@inu.ac.kr>
2025-04-11 10:40:57 +0900

    refactor: CafeThemeService에서 캐시 어노테이션 제거
    - @Cacheable(value = "cafeThemes") 어노테이션 제거
    - 캐시가 필요 없는 로직으로 판단되어 단순 호출로 변경

--- src/main/java/CafeFinder/cafe/service/impl/CafeThemeServiceImpl.java
+++ src/main/java/CafeFinder/cafe/service/impl/CafeThemeServiceImpl.java
@@ -6,7 +6,6 @@ import CafeFinder.cafe.service.interfaces.CafeThemeService;
 import java.util.Arrays;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +13,6 @@ import org.springframework.stereotype.Service;
 public class CafeThemeServiceImpl implements CafeThemeService {
 
     @Override
-    @Cacheable(value = "cafeThemes")
     public List<CafeThemeDto> getCafeThemes() {
         return Arrays.stream(CafeTheme.values())
                 .map(CafeThemeDto::fromEntity)
