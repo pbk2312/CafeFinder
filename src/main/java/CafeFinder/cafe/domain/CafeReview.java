c41eb42a4fee07f07b5842eff68450aa79bd639b
pbk2312 <pbk2312@inu.ac.kr>
2025-04-11 10:34:22 +0900

    refactor: 생성자 접근 제어자 변경
    - 기본 생성자 접근 제어자를 protected로 수정하여 외부 생성을 제한
    - 전체 생성자 접근 제어자를 private으로 수정하여 정적 팩토리 메서드(create)를 통한 생성만 허용

--- src/main/java/CafeFinder/cafe/domain/CafeReview.java
+++ src/main/java/CafeFinder/cafe/domain/CafeReview.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,8 +16,8 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CafeReview {
 
     @Id
