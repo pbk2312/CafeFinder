558bd349dbde28273a4dd7c7b684221aa8a38a3a
pbk2312 <pbk2312@inu.ac.kr>
2025-04-11 10:30:52 +0900

    feat: CafeSearchRepository에 cafeCode 기반 조회 메서드 추가
    - findByCafeCode(String cafeCode) 메서드 추가로 단일 카페 검색 지원

--- src/main/java/CafeFinder/cafe/infrastructure/elasticSearch/CafeSearchRepository.java
+++ src/main/java/CafeFinder/cafe/infrastructure/elasticSearch/CafeSearchRepository.java
@@ -1,6 +1,7 @@
 package CafeFinder.cafe.infrastructure.elasticSearch;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.annotations.Query;
@@ -16,5 +17,7 @@ public interface CafeSearchRepository extends ElasticsearchRepository<IndexedCaf
 
     @Query("{\"bool\": {\"filter\": {\"geo_distance\": {\"distance\": \"?1\", \"location\": {\"lat\": ?0, \"lon\": ?2}}}}}")
     List<IndexedCafe> findCafesNearLocation(double latitude, String distance, double longitude);
-    
+
+    Optional<IndexedCafe> findByCafeCode(String cafeCode);
+
 }
