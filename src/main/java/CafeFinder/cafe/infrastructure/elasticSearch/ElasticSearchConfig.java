a9af584fae0fae2046205e210d93d90e0551acd4
pbk2312 <pbk2312@inu.ac.kr>
2025-04-11 10:47:19 +0900

    chore : 개행 제거

--- src/main/java/CafeFinder/cafe/infrastructure/elasticSearch/ElasticSearchConfig.java
+++ src/main/java/CafeFinder/cafe/infrastructure/elasticSearch/ElasticSearchConfig.java
@@ -21,7 +21,6 @@ public class ElasticSearchConfig extends ElasticsearchConfiguration {
         return ClientConfiguration.builder()
                 .connectedTo(host)
                 .build();
-
     }
 
 }
