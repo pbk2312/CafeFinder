de6ce93e2935c327dc9696a7577a74124fa94fea
pbk2312 <pbk2312@inu.ac.kr>
2025-04-13 23:58:43 +0900

    feat: - 인증 최적화를 위해 findMemberAuthByEmail(String email) 메서드 추가 - MemberAuthProjection 인터페이스를 활용한 부분 조회 지원

--- src/main/java/CafeFinder/cafe/repository/MemberRepository.java
+++ src/main/java/CafeFinder/cafe/repository/MemberRepository.java
@@ -1,9 +1,14 @@
 package CafeFinder.cafe.repository;
 
+import CafeFinder.cafe.domain.Member;
+import CafeFinder.cafe.infrastructure.jwt.MemberAuthProjection;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import CafeFinder.cafe.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
     Optional<Member> findByEmail(String email);
+
+    Optional<MemberAuthProjection> findMemberAuthByEmail(String email);
+
 }
