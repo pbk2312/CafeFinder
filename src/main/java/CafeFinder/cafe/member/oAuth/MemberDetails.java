cac6cd2864f82d2a9df041d80710d71d465f53c8
pbk2312 <pbk2312@inu.ac.kr>
2025-04-18 00:22:10 +0900

    refactor : Oauth2 리팩토링
    - 간소화

---
+++ src/main/java/CafeFinder/cafe/member/oAuth/MemberDetails.java
@@ -0,0 +1,54 @@
+package CafeFinder.cafe.member.oAuth;
+
+import CafeFinder.cafe.member.domain.Member;
+import CafeFinder.cafe.member.domain.MemberRole;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+
+@Getter
+@Accessors(chain = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberDetails implements OAuth2User {
+
+    private String name;
+    private String email;
+
+    @Setter
+    private Map<String, Object> attributes;
+
+    @Setter
+    private MemberRole memberRole;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(memberRole.name()));
+    }
+
+    public static MemberDetails from(Member member) {
+        MemberDetails memberDetails = new MemberDetails();
+        memberDetails.name = member.getNickName();
+        memberDetails.email = member.getEmail();
+        return memberDetails;
+    }
+
+    @Builder
+    public MemberDetails(Map<String, Object> attributes, String nickName, String email
+    ) {
+        this.email = email;
+        this.attributes = attributes;
+        this.name = nickName;
+    }
+
+
+}
---
+++ src/main/java/CafeFinder/cafe/member/oAuth/MemberDetailsFactory.java
@@ -0,0 +1,43 @@
+package CafeFinder.cafe.member.oAuth;
+
+
+import CafeFinder.cafe.member.exception.UnavailableProviderException;
+import java.util.Map;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+@SuppressWarnings("all")
+public class MemberDetailsFactory {
+
+    public static MemberDetails memberDetails(String provider, OAuth2User oAuth2User) {
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        switch (provider.toUpperCase().trim()) {
+            case "GOOGLE" -> {
+                return MemberDetails.builder()
+                        .nickName(attributes.get("name").toString())
+                        .email(attributes.get("email").toString())
+                        .attributes(attributes)
+                        .build();
+            }
+            case "NAVER" -> {
+                Map<String, String> properties = (Map<String, String>) attributes.get("response");
+                return MemberDetails.builder()
+                        .nickName(properties.get("name"))
+                        .email(properties.get("id") + "@naver.com")
+                        .attributes(attributes)
+                        .build();
+            }
+            case "KAKAO" -> {
+                Map<String, String> properties = (Map<String, String>) attributes.get("properties");
+                return MemberDetails.builder()
+                        .nickName(properties.get("nickname"))
+                        .email(attributes.get("id").toString() + "@kakao.com")
+                        .build();
+            }
+            default -> throw new UnavailableProviderException(provider);
+        }
+
+    }
+
+}
