a392c431053cb89d7765cd213cbee506fdc68bc8
pbk2312 <pbk2312@inu.ac.kr>
2025-04-18 21:44:11 +0900

    feat: CafeFinderApplication에 @ConfigurationPropertiesScan 추가
    - @ConfigurationProperties를 사용하는 설정 클래스들을 자동으로 스캔할 수 있도록 @ConfigurationPropertiesScan 애노테이션을 추가
    - 설정 클래스를 등록하는 수고를 줄이고, 설정 자동 주입 기능을 강화함

--- src/main/java/CafeFinder/cafe/CafeFinderApplication.java
+++ src/main/java/CafeFinder/cafe/CafeFinderApplication.java
@@ -2,10 +2,12 @@ package CafeFinder.cafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@ConfigurationPropertiesScan
 public class CafeFinderApplication {
 
     public static void main(String[] args) {
