ef66f0a74edc262621471be912338127d17d99f8
pbk2312 <pbk2312@inu.ac.kr>
2025-04-29 18:04:35 +0900

    :recycle: refactor: accessToken 클래스명 변경

--- src/main/java/CafeFinder/cafe/member/dto/AccesTokenDto.java
+++ src/main/java/CafeFinder/cafe/member/dto/AccesTokenDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class AccesTokenInfoDto {
+public class AccesTokenDto {
 
     private String accessToken;
     private int accessTokenExpiresIn;
