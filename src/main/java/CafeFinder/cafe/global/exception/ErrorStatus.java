ce3ee07a1c43660e1e42e8b130f0d192d585a591
pbk2312 <pbk2312@inu.ac.kr>
2025-04-29 18:08:56 +0900

    :sparkles: feat: 예외 처리 응답 코드 추가

---
+++ src/main/java/CafeFinder/cafe/global/exception/ErrorStatus.java
@@ -0,0 +1,10 @@
+package CafeFinder.cafe.global.exception;
+
+public enum ErrorStatus {
+    BAD_REQUEST,
+    FORBIDDEN,
+    NOT_FOUND,
+    CONFLICT,
+    UNAUTHORIZED,
+    INTERNAL_SERVER_ERROR
+}
