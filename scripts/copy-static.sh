7560e12203637caea1341f7932ed15d29b058b69
pbk2312 <pbk2312@inu.ac.kr>
2025-05-08 22:58:40 +0900

    :recycle: feat: nginx 설정 수정

--- scripts/copy-static.sh
+++ scripts/copy-static.sh
@@ -3,8 +3,10 @@ set -eux
 
 # 1) nginx/html 초기화
 rm -rf nginx/html
-mkdir -p nginx/html
+mkdir -p nginx/html/static
 
 # 2) HTML 템플릿 복사
 cp src/main/resources/templates/*.html nginx/html/
 
+# 3) 정적 리소스 복사 (CSS, JS, 이미지 등)
+cp -r src/main/resources/static/* nginx/html/static/
